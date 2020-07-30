9bc4a9f658279dbe3773b1f116cbddb5644dfc64
prachi <prachisinghal1210@gmail.com>
2020-07-30 17:09:52 +0530

    new_Commit

---
+++ vectors.R
@@ -0,0 +1,23 @@
+#### VEctors
+
+## Numeric Vectors  #### Can have integers and floats
+vec = c(1L, 2, 4L, 6L)
+vec
+is.numeric(vec)
+is.integer(vec)
+is.double(vec)
+
+## Character Vectirs #### Can have single charater or string
+char_vec = c("a", 7, "bc")
+is.character(char_vec)
+
+## seq and replicate 
+a = seq(1,15)
+b = rep(1, 3)
+a = 1:15
+c= 1:15
+c[0]
+c
+seq(2,23, 2)
+3:12
+seq("a", "d")
